110941b284d3d370107b20729cfceec2805e80d5
Mathhew <Suwmlee@users.noreply.github.com>
2024-08-25 21:48:27 +0800

    chore: change pwd

--- frontend/src/views/settings/AccountSettingsSecurity.vue
+++ frontend/src/views/settings/AccountSettingsSecurity.vue
@@ -1,17 +1,26 @@
 <script lang="ts" setup>
+import { UsersService } from "@/client"
+import type { UpdatePassword } from "@/client/types.gen"
+
 const isCurrentPasswordVisible = ref(false)
 const isNewPasswordVisible = ref(false)
 const isConfirmPasswordVisible = ref(false)
-const currentPassword = ref('12345678')
-const newPassword = ref('87654321')
-const confirmPassword = ref('87654321')
+const currentPassword = ref("")
+const newPassword = ref("")
+const confirmPassword = ref("")
 
 const passwordRequirements = [
-  'Minimum 8 characters long - the more, the better',
-  'At least one lowercase character',
-  'At least one number, symbol, or whitespace character',
+  "Minimum 8 characters long - the more, the better",
 ]
 
+const changePasswd = async () => {
+  const data: UpdatePassword = {
+    current_password: currentPassword.value,
+    new_password: newPassword.value,
+  }
+  const response = await UsersService.updatePasswordMe({ requestBody: data })
+  console.log(response)
+}
 </script>
 
 <template>
@@ -23,53 +32,31 @@ const passwordRequirements = [
           <VCardText>
             <!-- 👉 Current Password -->
             <VRow>
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <!-- 👉 current password -->
-                <VTextField
-                  v-model="currentPassword"
-                  :type="isCurrentPasswordVisible ? 'text' : 'password'"
-                  :append-inner-icon="isCurrentPasswordVisible ? 'bx-hide' : 'bx-show'"
-                  label="Current Password"
+                <VTextField v-model="currentPassword" :type="isCurrentPasswordVisible ? 'text' : 'password'"
+                  :append-inner-icon="isCurrentPasswordVisible ? 'bx-hide' : 'bx-show'" label="Current Password"
                   placeholder="············"
-                  @click:append-inner="isCurrentPasswordVisible = !isCurrentPasswordVisible"
-                />
+                  @click:append-inner="isCurrentPasswordVisible = !isCurrentPasswordVisible" />
               </VCol>
             </VRow>
 
             <!-- 👉 New Password -->
             <VRow>
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <!-- 👉 new password -->
-                <VTextField
-                  v-model="newPassword"
-                  :type="isNewPasswordVisible ? 'text' : 'password'"
-                  :append-inner-icon="isNewPasswordVisible ? 'bx-hide' : 'bx-show'"
-                  label="New Password"
-                  autocomplete="on"
-                  placeholder="············"
-                  @click:append-inner="isNewPasswordVisible = !isNewPasswordVisible"
-                />
+                <VTextField v-model="newPassword" :type="isNewPasswordVisible ? 'text' : 'password'"
+                  :append-inner-icon="isNewPasswordVisible ? 'bx-hide' : 'bx-show'" label="New Password"
+                  autocomplete="on" placeholder="············"
+                  @click:append-inner="isNewPasswordVisible = !isNewPasswordVisible" />
               </VCol>
 
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <!-- 👉 confirm password -->
-                <VTextField
-                  v-model="confirmPassword"
-                  :type="isConfirmPasswordVisible ? 'text' : 'password'"
-                  :append-inner-icon="isConfirmPasswordVisible ? 'bx-hide' : 'bx-show'"
-                  label="Confirm New Password"
+                <VTextField v-model="confirmPassword" :type="isConfirmPasswordVisible ? 'text' : 'password'"
+                  :append-inner-icon="isConfirmPasswordVisible ? 'bx-hide' : 'bx-show'" label="Confirm New Password"
                   placeholder="············"
-                  @click:append-inner="isConfirmPasswordVisible = !isConfirmPasswordVisible"
-                />
+                  @click:append-inner="isConfirmPasswordVisible = !isConfirmPasswordVisible" />
               </VCol>
             </VRow>
           </VCardText>
@@ -81,17 +68,9 @@ const passwordRequirements = [
             </p>
 
             <ul class="d-flex flex-column gap-y-3">
-              <li
-                v-for="item in passwordRequirements"
-                :key="item"
-                class="d-flex"
-              >
+              <li v-for="item in passwordRequirements" :key="item" class="d-flex">
                 <div>
-                  <VIcon
-                    size="7"
-                    icon="bxs-circle"
-                    class="me-3"
-                  />
+                  <VIcon size="7" icon="bxs-circle" class="me-3" />
                 </div>
                 <span class="font-weight-medium">{{ item }}</span>
               </li>
@@ -100,13 +79,9 @@ const passwordRequirements = [
 
           <!-- 👉 Action Buttons -->
           <VCardText class="d-flex flex-wrap gap-4">
-            <VBtn>Save changes</VBtn>
+            <VBtn @click="changePasswd">Save changes</VBtn>
 
-            <VBtn
-              type="reset"
-              color="secondary"
-              variant="tonal"
-            >
+            <VBtn type="reset" color="secondary" variant="tonal">
               Reset
             </VBtn>
           </VCardText>
