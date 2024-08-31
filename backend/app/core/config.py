3d1b4f7604d60e27830373407f08c09c87d6399b
Mathhew <Suwmlee@users.noreply.github.com>
2024-09-01 02:26:08 +0800

    chore: add logging

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,5 @@
 
+import logging
 import secrets
 from pydantic_settings import BaseSettings
 
@@ -9,6 +10,10 @@ class Settings(BaseSettings):
     # 要与 alembic.ini 同步
     ALEMBIC_LOCATION: str = "./app/alembic"
     SQLALCHEMY_DATABASE_URI: str = "sqlite:///./data/db.sqlite3"
+    # 日志
+    LOGGING_FORMAT: str = "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
+    LOGGING_LOCATION: str = "./data/bonita.log"
+    LOGGING_LEVEL: int = logging.INFO
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 
+import logging
+from logging.handlers import RotatingFileHandler
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
@@ -33,6 +35,22 @@ def init_router():
     app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
+def log_config():
+    """
+    日志配置
+    """
+    max_log_size = 5 * 1024 * 1024  # 5 MB
+    backup_count = 5
+    formatter = logging.Formatter(settings.LOGGING_FORMAT)
+    handler = RotatingFileHandler(settings.LOGGING_LOCATION, maxBytes=max_log_size, backupCount=backup_count)
+    handler.setFormatter(formatter)
+    logging.basicConfig(
+        level=settings.LOGGING_LEVEL,
+        handlers=[handler]
+    )
+
+
+log_config()
 init_db()
 upgrade_db()
 init_router()
