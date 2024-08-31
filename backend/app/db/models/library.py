107f7904758229c522e3fbf9ba24679bd189c1d8
Mathhew <Suwmlee@users.noreply.github.com>
2024-08-31 14:10:08 +0800

    chore: update database

--- backend/app/db/models/library.py
+++ backend/app/db/models/library.py
@@ -7,20 +7,20 @@ from app.core.db import Base
 class Library(Base):
     """
     媒体库表
-    lib_mode:
-    1. 仅移动
-    2. 移动并且刮削
+    lib_type:
+    1. 移动
+    2. 整理?
 
     link_type:
-    :1  移动文件
-    :2  硬链接
+    :1  硬链接
+    :2  移动文件
     """
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, default='movie')
     description = Column(String, default='')
     lib_type = Column(Integer, default=1)
-    is_enabled = Column(Boolean, default=True)
-    auto_watch = Column(Boolean, default=False)
+    enabled = Column(Boolean, default=True)
+    auto_watch = Column(Boolean, default=False, comment="开启自动监测")
 
     link_type = Column(Integer, default=1)
     source_folder = Column(String, default='/media')
@@ -28,9 +28,10 @@ class Library(Base):
     failed_folder = Column(String, default='/media/failed')
 
     escape_folder = Column(String, default='Sample,sample')
-    escape_literals = Column(String, default="\()/")
+    escape_literals = Column(String, default="\\()/")
     escape_size = Column(Integer, default=0)
+    threads_num = Column(Integer, default=5)
 
     # 仅在刮削模式下生效,刮削配置
-    lib_scid = Column(String, default="")
-    threads_num = Column(Integer, default=5)
+    sc_enabled = Column(Boolean, default=False, comment="启用刮削")
+    sc_id = Column(String, default="", comment="使用的刮削配置")
