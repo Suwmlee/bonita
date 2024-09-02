d7244d01ae6853746e245ce86d8e69aac99e0b3b
Mathhew <Suwmlee@users.noreply.github.com>
2024-09-03 00:22:34 +0800

    chore: get task

--- backend/app/schemas/task.py
+++ backend/app/schemas/task.py
@@ -5,9 +5,24 @@ from pydantic import BaseModel
 # Shared properties
 class TaskBase(BaseModel):
     name: str
-
+    description: str
+    task_type: int = 1
+    enabled: bool = True
+    auto_watch: bool = False
+    transfer_type: int = 1
+    source_folder: Optional[str] = None
+    output_folder: Optional[str] = None
+    failed_folder: Optional[str] = None
+    escape_folder: Optional[str] = None
+    escape_literals: Optional[str] = None
+    escape_size: Optional[int] = 1
+    threads_num: Optional[int] = 1
+    sc_enabled: bool = False
+    sc_id: Optional[int] = None
 
 # Properties to return via API, id is always required
+
+
 class TaskPublic(TaskBase):
     id: int
 
@@ -21,8 +36,7 @@ class TasksPublic(BaseModel):
 
 
 class TaskCreate(TaskBase):
-    description: str
-    task_type: int
-
-    sc_enabled: bool
-    sc_id: Optional[int] = None
+    scraping_sites: Optional[str] = None
+    location_rule: Optional[str] = None
+    naming_rule: Optional[str] = None
+    max_title_len: Optional[str] = None
