b791a2e54263be33d2e9d1609a0293e2f3b165b5
Mathhew <Suwmlee@users.noreply.github.com>
2024-09-18 17:15:57 +0800

    chore: custom celery state

--- backend/app/celery_tasks/tasks.py
+++ backend/app/celery_tasks/tasks.py
@@ -6,7 +6,7 @@ from app import schemas
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 3},
              name='transfer:shared')
 def celery_transfer(self, task_json):
-    print("app_task start")
+    self.update_state(state="PROGRESS", meta={"progress": 0, "step": "transfer task: start"})
     task_info = schemas.TransferTaskPublic(**task_json)
     print("app_task end")
     data = True
